b4457f804697c3462c42e744fa39a767221c0f4b
Sam J <jacobsamro@outlook.com>
2021-03-14 21:29:13 +0530

    fix(setup): update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,23 @@ module.exports = {
   },
   extends: [
     '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    "vue/html-self-closing": "off",
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/multiline-html-element-content-newline": "off",
+    "quotes": "off",
+    "quote-props": "off",
+    "semi": "off",
+    "padded-blocks": "off",
+    "space-before-function-paren": "off",
+    "no-console": "off",
+    "comma-dangle": "off",
+    "arrow-parens": "off",
+    "no-multiple-empty-lines": "off",
+    "vue/no-v-html": "off",
+  },
 }
